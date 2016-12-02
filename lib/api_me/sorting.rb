baed01e5c758f5ece229e98b8f845a3a5cc32c34
Nathan Ockerman <ockerman.ns@gmail.com>
2016-12-02 13:17:48 -0800

    Updated sorting to alow for model association sorting.

--- lib/api_me/sorting.rb
+++ lib/api_me/sorting.rb
@@ -5,13 +5,13 @@ module ApiMe
     def initialize(scope:, sort_params:)
       self.scope = scope
       if sort_params
-        self.sort_criteria = sort_params[:criteria]
+        self.sort_criteria = sort_params[:criteria] || default_sort_criteria
         self.sort_reverse = sort_params[:reverse]
       end
     end
 
     def results
-      sorting? ? sort.scope : scope
+      sorting? ? sort(sort_criteria) : scope
     end
 
     def sort_meta
@@ -26,20 +26,12 @@ module ApiMe
 
     protected
 
-    def sort
-      unless sort_criteria === ""
-        sort_p = sort_criteria
-        if sort_reverse === "true"
-          self.scope = scope.sort_by {|scope| scope[sort_p]}.reverse!
-        else
-          self.scope = scope.sort_by {|scope| scope[sort_p]}
-        end
-        self.scope
+    def sort(criteria = default_sort_criteria)
+      if sort_reverse === "true"
+        self.scope = scope.order(criteria => :asc)
       else
-        default_sort_criteria
-        sort.scope
+        self.scope = scope.order(criteria => :desc)
       end
-      self
     end
 
     private
