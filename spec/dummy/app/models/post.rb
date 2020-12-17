aaa5e123d5613811756211de2d54aa9c969346e0
Joe Weakley <joew@samjoe.com>
2020-12-17 15:25:58 -0800

    CSV Streaming
    Added CSV streaming support to index routes.

--- spec/dummy/app/models/post.rb
+++ spec/dummy/app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :post_tags
   has_many :tags, through: :post_tags
+
+  def to_csv
+    [id, name]
+  end
+
+  def csv_headers
+    ['id', 'name']
+  end
 end
